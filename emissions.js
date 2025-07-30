23a18c1cfe91e0348522e664a19bea42601d1e85
Rachel <78095303+rachelm27@users.noreply.github.com>
2025-07-30 16:45:16 -0500

    title

--- emissions.js
+++ emissions.js
@@ -1,5 +1,6 @@
 async function page1() {
     document.getElementById("prev").disabled = true;
+    document.getElementById("chart-title") = "Section 1: Worldwide Greenhouse Gas Emissions by Sector";
     var data = d3.csv("https://ourworldindata.org/grapher/ghg-emissions-by-sector.csv?v=1&csvType=full&useColumnShortNames=true", function(d) {
     d.Year = +d.Year;
     d.agriculture_ghg_emissions = +d.agriculture_ghg_emissions;
