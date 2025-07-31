75008c4c401f541cf0bfabd36367c97700e65585
Rachel <78095303+rachelm27@users.noreply.github.com>
2025-07-30 19:19:15 -0500

    add to tooltip

--- water-use.js
+++ water-use.js
@@ -36,13 +36,16 @@ async function page2() {
 
         function getPercentTotal(colName, year) {
             var yearData = worldData.filter(d => d.Year === year);
-            console.log(yearData);
+            // console.log(yearData);
 
             if (colName === "agricultural_water_withdrawal") {
+                console.log(yearData.agricultural_water_withdrawal_as_percent_of_total_water_withdrawal);
                 return yearData.agricultural_water_withdrawal_as_percent_of_total_water_withdrawal;
             } else if (colName === "industrial_water_withdrawal") {
+                console.log(yearData.industrial_water_withdrawal_as_percent_of_total_water_withdrawal);
                 return yearData.industrial_water_withdrawal_as_percent_of_total_water_withdrawal;
             } else if (colName === "municipal_water_withdrawal") {
+                console.log(yearData.municipal_water_withdrawal_as_percent_of_total_withdrawal);
                 return yearData.municipal_water_withdrawal_as_percent_of_total_withdrawal;
             }
         }
