5f104cededb52056fac68b5deb65384d8a6d37c6
Rachel <78095303+rachelm27@users.noreply.github.com>
2025-08-02 19:03:26 -0500

    page 3

--- food.js
+++ food.js
@@ -14,8 +14,8 @@ async function page3() {
         
         var categories = ["Fruits", "Grains", "Meats/Animal Products", "Sugars", "Vegetables", "Dairy", "Nuts", "Legumes", "Other"]
         // https://observablehq.com/@d3/color-schemes
-        const colors = ["#ff725c","#efb118","#4269d0","#6cc5b0","#3ca951","#ff8ab7","#a463f2","#97bbf5", "#ed313aff"]
-        const hoverColors = ["#c3503fff","#b58303ff","#294798ff","#549e8cff","#2b873dff","#c35e84ff","#773bc1ff","#6086c3ff", "#ba242bff"]
+        const colors = ["#ff725c","#efb118","#97bbf5","#6cc5b0","#3ca951","#ff8ab7","#a463f2","#4269d0", "#ed313aff"]
+        const hoverColors = ["#c3503fff","#b58303ff","#6086c3ff","#549e8cff","#2b873dff","#c35e84ff","#773bc1ff","#294798ff", "#ba242bff"]
 
         function assignColor(entity) {
             const curData = data.filter(d => d.Entity === entity);
