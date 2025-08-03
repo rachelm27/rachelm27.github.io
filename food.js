fc77c3b49a50d911cc29d42c33a73f791b0bd51f
Rachel <78095303+rachelm27@users.noreply.github.com>
2025-08-03 00:36:24 -0500

    annotation 4

--- food.js
+++ food.js
@@ -166,7 +166,7 @@ async function page3() {
             label: "All fruits and vegetables use less than 420 liters of water and produce less than 2.1 kilograms of GHG emissions.",
             title: ""
         },
-        data: { Emissions: 0.5, Water: 135 },
+        data: { Emissions: 0.6, Water: 135 },
         dy: 195,
         dx: -19,
         subject: {
